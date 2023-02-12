2e51ad7cb477b8ac6b6cfbc82abbcd033d8ceaad
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-12 10:30:04 +0530

    Day 1(DSA)

--- stacks.java
+++ stacks.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class stacks {
+import java.util.Stack;
+
+public class stacks {
+    public static void main(String[] args) {
+        Stack <String> books = new Stack<>();
+        System.out.println(books.isEmpty());
+        books.push("Rich Dad Poor Dad");
+        books.push("Atomic Habits");
+        books.push("Zero to One");
+        books.push("Verity");
+
+        int item = books.search("Verity");
+        String last = books.pop();//we can store pop value in any variable to use it for further use
+
+        System.out.println(books);
+        System.out.println(books.isEmpty()); // returns true or false
+        System.out.println(item);// As Verity is last added item its position will be 1 and last added item that is Richdad will be 4
+        System.out.println(books.search("Rich Dad Poor Dad"));
+        System.out.println(books.search("The Art ___"));/// If element is not present then returns -1
+        System.out.println(last);/// If element is not present then returns -1
+        System.out.println(books.peek());
+
+    }
 }
