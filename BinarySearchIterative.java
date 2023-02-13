ed08ccbf6bafcc601497209efd48bde82b4600e5
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-13 19:27:28 +0530

    Day 2(DSA)

---
+++ BinarySearchIterative.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class BinarySearchIterative {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(array));
+        int key = 6;
+        int start = 0;
+        int end = array.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            int midValue = array[mid];
+            if(mid == key) System.out.println("Found at "+(mid-1));
+            if (key>midValue) start = mid+1;
+            if(key<midValue) end = mid-1;
+        }
+    }
+}
